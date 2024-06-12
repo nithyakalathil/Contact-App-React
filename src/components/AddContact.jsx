83c5b62f256c4f7f0bc654d065e6bc684faf2dd3
nithyakalathil <nithya913k@gmail.com>
2024-06-13 04:43:51 +0530

    create

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const AddContact = () => {
     
@@ -36,7 +37,7 @@ const readValue=()=>{
 }
   return (
     <div>
-
+<Navbar/>
 <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
